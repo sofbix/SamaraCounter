924b720c185f534c26e0f6dd3f16651b9e1093b6
Sergey Balalaev <sof.bix@mail.ru>
2021-10-29 22:52:02 +0400

    add logic warnings

--- SamaraCounter/Network/EsPlusSendDataService.swift
+++ SamaraCounter/Network/EsPlusSendDataService.swift
@@ -28,7 +28,7 @@ struct EsPlusSendDataService : SendDataService {
             "Cookie" : "PHPSESSID=rs3574vh1dlgq8buc00lmhota4; BITRIX_SM_REGION=samara; BITRIX_SM_GUEST_ID=30347255; BITRIX_SM_LAST_VISIT=24.12.2020+22%3A33%3A41; INVOLVE_SESSION_MONITOR=1; BITRIX_CONVERSION_CONTEXT_s1=%7B%22ID%22%3A7%2C%22EXPIRE%22%3A1608843540%2C%22UNIQUE%22%3A%5B%22conversion_visit_day%22%5D%7D; _ym_uid=1584365216943771550; _ym_d=1608838300; _ga=GA1.2.221495573.1608838300; _gid=GA1.2.1431768146.1608838300; _fbp=fb.1.1608838300564.409783820; BX_USER_ID=dab864944a1f7a0ce7716b01dbd35b77; _ym_isad=1"
         ]
         
-        #warning("Only First!")
+        #warning("Only First! Please fix it when you can test")
         
         let onlyFirstSN = input.waterCounters.first?.hotSerialNumberRow.value ?? ""
         let onlyFirstValue = input.waterCounters.first?.hotCountRow.value ?? ""
--- SamaraCounter/Network/SendDataService.swift
+++ SamaraCounter/Network/SendDataService.swift
@@ -16,6 +16,7 @@ struct ProgressService {
 
     func start(with title: String) -> Promise<Data> {
         return Promise { seal in
+            #warning("May be is sync? Please fixed and test! I think it can show only first message")
             DispatchQueue.main.async {
                 CircularSpinner.show(title, animated: true, type: .indeterminate, showDismissButton: false)
                 seal.fulfill(Data())
